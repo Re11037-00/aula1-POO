4a11719ab21611ba9005893cd2d2cf254eaf2a6d
Re11037-00 <santos.rebeka@escolar.ifrn.edu.br>
2026-03-17 17:21:57 +0000

    new file:   aula_02/exemplo_01.py 	new file:   aula_02/exemplo_02.py

---
+++ aula_02/exemplo_01.py
@@ -0,0 +1,8 @@
+print("Digite quatro valores inteiros")
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+'''soma_pares = 
+soma_impares = '''
---
+++ aula_02/exemplo_02.py
@@ -0,0 +1,18 @@
+print('Informe o número do mês')
+mes = int(input())
+
+match mes:
+    case 1: mes = 'Janeiro'
+    case 2: mes = 'Fevereiro'
+    case 3: mes = 'Fevereiro'
+    case 4: mes = 'Fevereiro'
+    case 5: mes = 'Fevereiro'
+    case 6: mes = 'Fevereiro'
+    case 7: mes = 'Fevereiro'
+    case 8: mes = 'Fevereiro'
+    case 9: mes = 'Fevereiro'
+    case 10: mes = 'Fevereiro'
+    case 11 mes = 'Fevereiro'
+
+
+print(mes)
